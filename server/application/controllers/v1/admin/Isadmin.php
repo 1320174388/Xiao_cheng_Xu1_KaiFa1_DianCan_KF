8229227916e33ef7eab4c2ac43eb420c4ebfaf6f
时广玉 <shiguangyu@dlaotianhuang.com>
2018-04-16 23:19:48 +0800

    201804162319_管理员模块更新

--- server/application/controllers/v1/admin/Isadmin.php
+++ server/application/controllers/v1/admin/Isadmin.php
@@ -24,4 +24,58 @@ class Isadmin extends CI_Controller {
             ]);
         }
     }
+    // 获取管理员列表信息
+    public function roles()
+    {
+        // 判断用户是不是系统最高管理员
+        if(is_system_admin()){
+            // 获取权限信息
+            $this->load->model('admin/Admin');
+            $res = $this->Admin->get_admin_roles();
+            // 返回结果
+            $this->json($res);
+        }else{
+            $this->json([
+                "errNum" => 1,
+                "errMsg" => "你没有权限进行此操作"
+            ]);
+        }
+    }
+    // 添加管理员
+    public function create()
+    {
+        // 判断是否传值了role_id
+        $role_id = $this->input->post('role_id');
+        if($role_id){
+            // 获取要添加的管理员名称
+            $admin_name = $this->input->post('admin_name');
+            if($admin_name){
+                // 判断用户是不是系统最高管理员
+                if(is_system_admin()){
+                    // 获取要添加成为管理员的用户ID
+                    $user_id = $this->input->post('user_id');
+                    // 添加管理员
+                    $this->load->model('admin/Admin');
+                    $res = $this->Admin->set_admin_user($user_id,$admin_name,$role_id);
+                    // 返回结果
+                    $this->json($res);
+                }else{
+                    $this->json([
+                        "errNum" => 3,
+                        "errMsg" => "你没有权限进行此操作"
+                    ]);
+                }
+            }else{
+                $this->json([
+                    "errNum" => 2,
+                    "errMsg" => "没有输入管理员名称"
+                ]);
+            }
+        }else{
+            $this->json([
+                "errNum" => 1,
+                "errMsg" => "没有选择管理员职位"
+            ]);
+        }
+    }
 }
--- server/application/models/admin/Admin.php
+++ server/application/models/admin/Admin.php
@@ -38,4 +38,78 @@ class Admin extends CI_Model {
             ];
         }
     }
+
+    public function get_admin_roles()
+    {
+        // 获取所有管理职位信息
+        $role_data = $this->CI->db->get('data_admin_roles');
+        if($role_data->result()){
+            return [
+                "errNum"  => 0,
+                "retMsg"  => "请求成功",
+                "retData" => [
+                    "list" => $role_data->result()
+                ]
+            ];
+        }else{
+            return [
+                "errNum" => 2,
+                "errMsg" => "当前还没有管理职位"
+            ];
+        }
+    }
+
+    public function set_admin_user($user_id,$admin_name,$role_id)
+    {
+        // 判断user_id对应用户是否存在
+        $home_user = $this->CI->db->get_where('data_home_users',['id'=>$user_id]);
+        if($home_user->result()){
+            // 判断管理员名称是否已经存在
+            $admin_user = $this->CI->db->get_where('data_admin_users',['admin_name'=>$admin_name]);
+            if(!$admin_user->result()){
+                // 添加数据库
+                $date = date('Y-m-d H:i:s',time());
+                $res = $this->CI->db->insert('data_admin_users',[
+                    'user_id'     => $user_id,
+                    'admin_name'  => $admin_name,
+                    'create_time' => $date,
+                    'update_time' => $date
+                ]);
+                if($res){
+                    $admin_user1 = $this->CI->db->get_where('data_admin_users',['admin_name'=>$admin_name]);
+                    $ret = $this->CI->db->insert('index_user_roles',[
+                        'admin_id'     => $admin_user1->result()[0]->id,
+                        'role_id'  => $admin_name,
+                    ]);
+                    if($ret){
+                        return [
+                            "errNum"  => 0,
+                            "retMsg"  => "添加成功",
+                            "retData" => []
+                        ];
+                    }else{
+                        return [
+                            "errNum" => 7,
+                            "errMsg" => "管理员职位绑定失败"
+                        ];
+                    }
+                }else{
+                    return [
+                        "errNum" => 6,
+                        "errMsg" => "管理员添加失败"
+                    ];
+                }
+            }else{
+                return [
+                    "errNum" => 5,
+                    "errMsg" => "管理员名称已存在"
+                ];
+            }
+        }else{
+            return [
+                "errNum" => 4,
+                "errMsg" => "用户不存在"
+            ];
+        }
+    }
 }
