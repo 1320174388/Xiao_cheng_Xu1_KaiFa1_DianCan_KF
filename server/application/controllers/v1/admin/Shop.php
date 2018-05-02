677b2ec7602ea0ac34d13958420a7b3b2a2e9d6e
时广玉 <shiguangyu@dlaotianhuang.com>
2018-05-02 14:48:44 +0800

    201805021448

--- server/application/controllers/v1/admin/Shop.php
+++ server/application/controllers/v1/admin/Shop.php
@@ -28,33 +28,6 @@ class Shop extends LoginController{
     }
 
     /**
-     * 添加店铺信息
-     *
-     * @access public
-     * @param string $shop_name 店铺名称
-     * @param string $shop_img 店铺图片
-     * @param string $shop_info 店铺信息
-     * @param string $shop_addr 店铺地址
-     * @param int $shop_status 店铺状态
-     * @param int $ordering_status 店铺接单状态
-     * @return array 中返回是否修改成功
-     */
-    public function add_info(){
-        $post = $this->input->post();
-        if( count( $post ) <= 0 ){
-            return return_response( '0', '参数错误', [] );
-        }
-
-        $result = $this->M_Shop->create( $post );
-
-        if( $result ){
-            return return_response( '1', '添加成功', $result );
-        }else{
-            return return_response( '0', '添加失败', $result );
-        }
-    }
-
-    /**
      * 更新店铺信息
      *
      * @access public
