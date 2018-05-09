52b52ec3f3b1c5ed6ac16d18da0c3b9a7dffbe9f
seven <lishaohua>
2018-05-09 17:40:59 +0800

    201805091740_修改点餐页面

--- client/pages/Home/OrderFood/orderMenu/index.js
+++ client/pages/Home/OrderFood/orderMenu/index.js
@@ -7,6 +7,21 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 轮播图
+    host: config.index.host,
+    imgUrls: null,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    // 走马灯
+    noticesCurrent: '0',
+    notices: [
+      { sid: 1, title: '点餐：满100元减10元' },
+      { sid: 2, title: '到店取餐：满100元减12元' },
+      { sid: 3, title: '新品即将来袭，你准备好了吗？' }
+    ],
+    // 导航条
     navbar: null,
     host:config.service.host,
     key2:null,
@@ -28,7 +43,13 @@ Page({
       currentTab: e.currentTarget.dataset.idx
     })
   },
-
+  //notices轮播切换事件
+  noticesChange: function (e) {
+    //修改notice swiper当前索引
+    this.setData({
+      noticesCurrent: e.detail.current
+    })
+  },
   // submitCart
   submitCart: function (e) {
     var key1 = this.data.currentTab;
@@ -102,6 +123,33 @@ Page({
    */
   onLoad: function (options) {
     var This = this;
+    // 图片轮播
+    app.post(
+      config.index.git_sowing_map, {
+      }, function (res) {
+        This.setData({
+          imgUrls: res.data.retData
+        });
+      }
+    );
+    // 登录获取token
+    // wx.login({
+    //   success: function (res) {
+    //     if (res.code) {
+    //       app.post(
+    //         config.service.cheshiUrl, {
+    //           code: res.code
+    //         }, function (res) {
+    //           console.log(res.data);
+    //           wx.setStorageSync('token', res.data.retData.token);
+    //         }
+    //       );
+    //     } else {
+    //       console.log('登录失败' + res.errMsg);
+    //     };
+    //   }
+    // });
+    // 菜品
     This.setData({
       food_info_arr: wx.getStorageSync('food_info_arr'),
     });
