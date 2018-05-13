40229278335cb810f2f7dd454628312f1fdba010
时广玉 <shiguangyu@dlaotianhuang.com>
2018-05-14 00:57:16 +0800

    201805140057_完善微信支付

--- client/app.js
+++ client/app.js
@@ -139,11 +139,18 @@ App({
 });
 login_add();
 // 用户登录信息
-function login_add() {
+function login_add(number = 1) {
   setInterval(function () {
+    if(number==1){
+      wx.removeStorageSync('token');
+      number++;
+    }
     if (wx.getStorageSync('token')) {
       return false;
     }
+    setTimeout(function(res){
+      wx.removeStorageSync('token');
+    },3600000);
     wx.login({
       success: function (res) {
         if (res.code) {
