7de36d47f5c6983819184035f5f0f263b0a18006
seven <lishaohua>
2018-06-12 15:41:47 +0800

    201806121255-点餐开发图片页面优化

--- client/config.js
+++ client/config.js
@@ -3,6 +3,7 @@
  */
 // 此处主机域名修改成腾讯云解决方案分配的域名
 var host = 'https://l1ur0jdu.qcloud.la';
+var imgUrl = 'https://lg-3fm9mghs-1253290031.cos.ap-shanghai.myqcloud.com/';
 var config = {
     index:{
       host,
@@ -25,6 +26,7 @@ var config = {
     // 下面的地址配合云端 Demo 工作
     service: {
         host,
+        imgUrl,
         // 测试登录接口,获取令牌
         cheshiUrl: `${host}/api/home/login`,
         // 请求用户数据，必须发送令牌
